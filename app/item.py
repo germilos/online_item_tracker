4e87e31236803fe9762ddc49b6f8e64bde1516fe
Gerun <milos.gerun@zuehlke.com>
2021-04-21 08:10:02 +0200

    Converting item to track from json request

--- app/item.py
+++ app/item.py
@@ -1,4 +1,5 @@
 import json
+from types import SimpleNamespace
 
 
 class Item:
@@ -18,3 +19,21 @@ class Item:
 
     def to_json(self):
         return json.dumps(self, indent=4, default=lambda o: o.__dict__)
+
+
+class ItemToTrack:
+    def __init__(self, url, price, available, additional_info):
+        self.url = url
+        self.price = price
+        self.available = available
+        # Some sort of map - similar to object in JS
+        self.additional_info = additional_info
+
+    def __repr__(self):
+        return f"Price: {self.price!r}, Available: {self.available!r})"
+
+    def __str__(self):
+        return f"Price: {self.price}, Available: {self.available}"
+
+    def from_json(data):
+        return json.loads(data, object_hook=lambda d: SimpleNamespace(**d))
--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,25 @@
-from flask import request
+from flask import request, make_response
 from flask import jsonify
 from app import app
 from app import item_scraper
 from app.errors import InvalidUsageError
+from app.item import ItemToTrack
 
 
-@app.route('/item')
+@app.route('/item-info')
 def get_item_info():
     url = request.args.get('url')
     item = item_scraper.scrape(url)
     return item.to_json()
+
+
+@app.route('/items', methods=['POST'])
+def track_item():
+    # Get convert request to item
+    item_to_track = ItemToTrack.from_json(request.data)
+    # TODO: Put item in database
+
+    return f'Item with url: {item_to_track.url} is now being tracked!'
 
 
 @app.errorhandler(InvalidUsageError)
