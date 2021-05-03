a55f46d9eafc3a77fad1a7f58f97005a40dac3c7
Gerun <milos.gerun@zuehlke.com>
2021-05-03 18:39:04 +0200

    Added vcs to tests

--- tests/test_onlineitemtracker.py
+++ tests/test_onlineitemtracker.py
@@ -1,9 +1,11 @@
 from app.item_scraper import scrape
+import pytest
 
+
+@pytest.mark.vcr()
 def test_should_scrape_metropolis_music_online_item_info_when_given_item_url():
     item = scrape('https://www.metropolismusic.rs/vs.6218155.html')
     assert item.url == 'https://www.metropolismusic.rs/vs.6218155.html'
     assert item.name == 'Vs. - Pearl Jam'
     assert item.price.amount == 3499.00
     assert item.available is True
-
