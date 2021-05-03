fddf6058b3c43c3e58dcfbd0262f89cdfcbb1946
Gerun <milos.gerun@zuehlke.com>
2021-05-03 21:07:43 +0200

    Zara scraping in progress

--- app/item_scraper.py
+++ app/item_scraper.py
@@ -9,7 +9,8 @@ from app.util import CurrentPrice
 
 # General scraping steps
 def scrape(url):
-    page = requests.get(url)
+    user_agent = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}
+    page = requests.get(url, headers=user_agent)
     parsed_page = BeautifulSoup(page.text, 'html.parser')
     website_name = extract_website_name(url)
     try:
@@ -38,6 +39,26 @@ def scrape_metropolis_music(url, page_to_scrap):
     return Item(url, name, price, available, {})
 
 
+def scrape_zara(url, page_to_scrap):
+    container = page_to_scrap.find('div', class_='product-detail-info')
+    if container is None:
+        raise NoHTMLElementFoundError()
+
+    name = container.h1.string
+    price_span = container.find('span', class_='price__amount').string
+    amount_and_currency = price_span.split()
+    price = CurrentPrice(amount=float(amount_and_currency[0].replace(',', '')), currency=amount_and_currency[1])
+    available = False
+
+    selected_color_container = container.find('div', class_='product-detail-color-selector')
+    selected_color = selected_color_container.p.contents[3]
+
+    selected_size_container = container.find('div', class_='product-size-selector')
+    size = selected_size_container.span.string
+
+    return Item(url, name, price, available, {'color': selected_color, 'size': size})
+
+
 def extract_website_name(url):
     domain = urlparse(url).netloc
     stripped_domain = domain[domain.index('.') + 1:domain.rfind('.')]
@@ -47,5 +68,6 @@ def extract_website_name(url):
 
 #  TODO: Populate scraping functions map
 scrape_map = {
-    'metropolismusic': scrape_metropolis_music
+    'metropolismusic': scrape_metropolis_music,
+    'zara': scrape_zara
 }
