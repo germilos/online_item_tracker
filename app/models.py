432ca183f7d52f0a619e1fcaf08d49eaba85e54f
Gerun <milos.gerun@zuehlke.com>
2021-05-14 09:28:08 +0200

    Added User class and implemented jwt encoding/decoding

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 import json
 import mongoengine as me
+import jwt
+import datetime
 from app import app, bcrypt
 
 
@@ -22,6 +24,17 @@ class Item:
         return json.dumps(self, indent=4, default=lambda o: o.__dict__)
 
 
+class Price(me.EmbeddedDocument):
+    amount = me.DecimalField(required=True)
+    currency = me.StringField(required=True)
+
+    def __repr__(self):
+        return f"{self.price!r} {self.currency!r})"
+
+    def __str__(self):
+        return f"{self.amount} {self.currency}"
+
+
 class ItemToTrack(me.Document):
     url = me.StringField(required=True)
     price = me.EmbeddedDocumentField(Price)
@@ -33,17 +46,6 @@ class ItemToTrack(me.Document):
 
     def __str__(self):
         return f"Price: {self.price}, Available: {self.available}"
-
-
-class Price(me.EmbeddedDocument):
-    amount = me.DecimalField(required=True)
-    currency = me.StringField(required=True)
-
-    def __repr__(self):
-        return f"{self.price!r} {self.currency!r})"
-
-    def __str__(self):
-        return f"{self.amount} {self.currency}"
 
 
 class CurrentPrice:
@@ -61,10 +63,36 @@ class CurrentPrice:
 class OnlineShopper(me.Document):
     email = me.EmailField(required=True)
     password = me.StringField(required=True)
-    registered_on=me.DateTimeField()
+    registered_on = me.DateTimeField()
 
     def __init__(self, email, password, registered_on):
         self.email = email
         self.password = bcrypt.generate_password_hash(
             password, app.config.get('BCRYPT_LOG_ROUNDS')
         ).decode()
+        self.registered_on = registered_on
+
+    def encode_auth_token(self, user_id):
+        try:
+            payload = {
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=5),
+                'iat': datetime.datetime.utcnow(),
+                'sub': user_id
+            }
+            return jwt.encode(
+                payload,
+                app.config.get('SECRET_KEY'),  # TODO: Replace with key from config
+                algorithm='HS256'
+            )
+        except Exception as e:
+            return e
+
+    @staticmethod
+    def decode_auth_token(auth_token):
+        try:
+            payload = jwt.decode(auth_token, app.config.get('SECRET_KEY'))
+            return payload['sub']
+        except jwt.ExpiredSignatureError:
+            return 'Signature expired. Please log in again.'
+        except jwt.InvalidTokenError:
+            return 'Invalid token. Please log in again.'
